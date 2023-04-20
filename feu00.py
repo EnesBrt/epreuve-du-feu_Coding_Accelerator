5ebf3fd6810ad67f9ce3803375a7694f7dcbfb7f
EnesBrt <enes.barut@gmx.com>
2023-04-20 16:32:42 +0200

    added feu00.py

---
+++ feu00.py
@@ -0,0 +1,30 @@
+# échauffement
+
+import sys
+
+
+def training(columns, rows):
+    for row in range(rows):
+        for col in range(columns):
+            if (row, col) == (0, 0) or (row, col) == (0, columns - 1) or (row, col) == (rows - 1, 0) or (row, col) == (
+                    rows - 1, columns - 1):
+                print("O", end="")
+            elif col == 0 or col == columns - 1:
+                print("|", end="")
+            elif row == 0 or row == rows - 1 and col != 0 and col != columns - 1:
+                print("-", end="")
+            else:
+                print(" ", end="")
+        print()
+
+
+try:
+    argument_one = int(sys.argv[1])
+    argument_two = int(sys.argv[2])
+    training(argument_one, argument_two)
+except IndexError:
+    print("error !")
+except ValueError:
+    print("error !")
+except TypeError:
+    print("error !")

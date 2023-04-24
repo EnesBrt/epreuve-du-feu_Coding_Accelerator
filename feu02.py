445e3056b52582026f6cd5550cf590c1fccbd4d0
EnesBrt <enes.barut@gmx.com>
2023-04-24 17:46:13 +0200

    added feu02.py

--- feu02.py
+++ feu02.py
@@ -15,19 +15,6 @@ print(board)
 print(to_find)
 
 
-# Trouver une forme
-
-def read_file_to_2d_list(file_path):
-    grid = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            grid.append(list(line.strip()))
-    return grid
-
-
-
-board = read_file_to_2d_list('board.txt')
-to_find = read_file_to_2d_list('to_find.txt')
 
 
 

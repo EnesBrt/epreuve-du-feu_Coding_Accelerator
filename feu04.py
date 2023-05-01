81d14997ecf27de65424927ca276c3af8559ac93
EnesBrt <enes.barut@gmx.com>
2023-05-01 13:04:22 +0200

    updated feu04.py and added generate_plateau.py

--- feu04.py
+++ feu04.py
@@ -1,25 +1,78 @@
 # Trouver le plus grand carré
 
 import sys
-import random
 
 
-def generate_plateau(x, y, density):
-    plateau = []
-    for i in range(y + 1):
-        row = ""
-        for j in range(x + 1):
-            row += 'x' if random.randint(0, y) * 2 < density else '.'
-        plateau.append(row)
+def read_plateau(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+    plateau = [list(line.strip()) for line in lines[1:]]
     return plateau
 
 
-arg_x = int(sys.argv[1])
-arg_y = int(sys.argv[2])
-arg_density = int(sys.argv[3])
+def is_valid_square(plateau, top_left_row, top_left_col, size):
+    for n in range(top_left_row, top_left_row + size):
+        for m in range(top_left_col, top_left_col + size):
+            if plateau[n][m] == 'x':
+                return False
+    return True
 
-if len(sys.argv) != 4:
-    print("params needed: x y density")
+
+def greater_square_calculator(plateau):
+    if not plateau:
+        return None, None, 0
+
+    row = len(plateau)
+    column = len(plateau[0])
+
+    max_square_row = -1
+    max_square_col = -1
+    max_square_size = 0
+
+    for size in range(min(row, column), 0, -1):
+        for n in range(row - size + 1):
+            for m in range(column - size + 1):
+                if is_valid_square(plateau, n, m, size):
+                    max_square_row = n
+                    max_square_col = m
+                    max_square_size = size
+                    return max_square_row, max_square_col, max_square_size
+
+    return max_square_row, max_square_col, max_square_size
+
+
+def is_square_empty(plateau, row, col, size):
+    for i in range(row, row + size):
+        for j in range(col, col + size):
+            if plateau[i][j] == 'x':
+                return False
+    return True
+
+
+def mark_greater_square(plateau, top_left_position, square_size):
+    for n in range(top_left_position[0], top_left_position[0] + square_size):
+        for m in range(top_left_position[1], top_left_position[1] + square_size):
+            if 0 <= n < len(plateau) and 0 <= m < len(plateau[0]):
+                if plateau[n][m] == '.':
+                    plateau[n][m] = 'o'
+
+
+def display_result(plateau, top_left_position, square_size):
+    if top_left_position[0] is not None and top_left_position[1] is not None:
+        mark_greater_square(plateau, top_left_position, square_size)
+
+    for row in plateau:
+        print("".join(row))
+
+
+if len(sys.argv) != 2:
+    print("params needed: plateau_filename")
     sys.exit()
 
-print(f"{arg_y}.xo")
+plateau_filename = sys.argv[1]
+plateau = read_plateau(plateau_filename)
+
+top_left_position_row, top_left_position_col, max_square_size = greater_square_calculator(plateau)
+top_left_position = (top_left_position_row, top_left_position_col)
+
+display_result(plateau, top_left_position, max_square_size)
---
+++ generate_plateau.py
@@ -0,0 +1,28 @@
+import random
+import sys
+
+
+def generate_plateau(x, y, density):
+    plateau = []
+    for i in range(y + 1):
+        row = ""
+        for j in range(x + 1):
+            row += 'x' if random.randint(0, y) * 2 < density else '.'
+        plateau.append(row)
+    return plateau
+
+
+if len(sys.argv) != 4:
+    print("params needed: x y density")
+    sys.exit()
+
+arg_x = int(sys.argv[1])
+arg_y = int(sys.argv[2])
+arg_density = int(sys.argv[3])
+
+plateau = generate_plateau(arg_x, arg_y, arg_density)
+
+with open("plateau", "w") as f:
+    f.write(f"{arg_y}.xo\n")
+    for row in plateau:
+        f.write(row + "\n")
